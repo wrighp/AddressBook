8db3fb1602f24209ac0a8855c8533cc157d555d5
wrighp <wrightcoder@gmail.com>
2019-07-10 10:36:42 -0400

    🔧 Swashbuckle.AspNetCore (4.0.1)

--- AddressBook/AddressBook/Startup.cs
+++ AddressBook/AddressBook/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using AddressBook.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AddressBook
 {
@@ -36,8 +37,15 @@ namespace AddressBook
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            //Add Databases
             services.AddDbContext<AddressBookContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("AddressBookContext")));
+
+            //Add Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +64,16 @@ namespace AddressBook
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
